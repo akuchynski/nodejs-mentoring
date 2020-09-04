5e67b2af69d07be6a995ef50f5527739d1813174
Andrei Kuchynski <andrei_kuchynski1@epam.com>
2020-09-04 14:34:16 +0300

    refactor user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,56 +1,75 @@
 const { models } = require('../db');
 
-async function create(req, res) {
-    const user = await models.user.findOne({ where: { login: req.body.login } });
-    if (user) {
-        res.status(409).send(`User login ${req.body.login} already exists!`);
-    }
+async function create(req, res, next) {
     try {
-        await models.user.create(req.body);
-        res.status(201);
+        const user = await models.user.findOne({
+            where: {
+                login: req.body.login
+            }
+        });
+        if (user) {
+            res.status(409).send(`User login ${req.body.login} already exists!`);
+        } else {
+            const userCreated = await models.user.create(req.body);
+            res.status(201).json(userCreated.id);
+        }
     } catch (error) {
-        console.log(error);
+        return next(error);
     }
 }
 
-async function getAll(req, res) {
-    const users = await models.user.findAll();
-    res.status(200).json(users);
-}
-
-async function getById(req, res) {
-    const user = await models.user.findByPk(req.params.id);
-    if (user) {
-        res.status(200).json(user);
-    } else {
-        res.status(404).send(`User with id ${req.params.id} not found!`);
-    }
-}
-
-async function update(req, res) {
-    const user = await models.user.findByPk(req.params.id);
-    if (!user) {
-        res.status(404).send(`User with id ${req.params.id} not found!`);
-    }
+async function getAll(req, res, next) {
     try {
-        await models.user.update(req.body, {
+        const users = await models.user.findAll();
+        res.json(users);
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function getById(req, res, next) {
+    try {
+        const user = await models.user.findByPk(req.params.id);
+        if (user) {
+            res.json(user);
+        } else {
+            res.status(404).send(`User with id ${req.params.id} not found!`);
+        }
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function update(req, res, next) {
+    try {
+        const user = await models.user.findByPk(req.params.id);
+        if (!user) {
+            res.status(404).send(`User with id ${req.params.id} not found!`);
+        } else {
+            await models.user.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            await user.reload();
+            res.json(user);
+        }
+    } catch (error) {
+        return next(error);
+    }
+}
+
+async function remove(req, res, next) {
+    try {
+        await models.user.destroy({
             where: {
                 id: req.params.id
             }
         });
-        res.status(200);
+        res.status(200).end();
     } catch (error) {
-        console.log(error);
+        return next(error);
     }
-}
-
-async function remove(req, res) {
-    await models.user.destroy({
-        where: {
-            id: req.params.id
-        }
-    });
-    res.status(200);
 }
 
 module.exports = {
