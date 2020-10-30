946c1c01b2c82b367dc37dc40da199c705ac6f31
Andrei Kuchynski <andrei_kuchynski1@epam.com>
2020-10-30 16:48:14 +0300

    split controllers unit tests to groups

--- api/controllers/__tests__/groupController.test.js
+++ api/controllers/__tests__/groupController.test.js
@@ -14,114 +14,126 @@ describe('Group Controller Tests', () => {
         jest.clearAllMocks();
     });
 
-    it('should create a new group #create', async () => {
-        req.body = { name: 'group123' };
-        const group = { id: 'uuid', ...req.body };
-        groupService.createGroup.mockImplementation(() => Promise.resolve(group));
-        await create(req, res, null);
-        expect(res.json).toBeCalledWith(group.id);
-        expect(groupService.createGroup).toBeCalledWith(req.body);
-        expect(res.status).toBeCalledWith(201);
+    describe('#create function', () => {
+        it('should create a new group', async () => {
+            req.body = { name: 'group123' };
+            const group = { id: 'uuid', ...req.body };
+            groupService.createGroup.mockImplementation(() => Promise.resolve(group));
+            await create(req, res, null);
+            expect(res.json).toBeCalledWith(group.id);
+            expect(groupService.createGroup).toBeCalledWith(req.body);
+            expect(res.status).toBeCalledWith(201);
+        });
+
+        it('should throw error (Conflict 409) if group exists', async () => {
+            const error = createError.Conflict();
+            req.body = { name: 'group123' };
+            groupService.createGroup.mockImplementation(() => Promise.reject(error));
+            await create(req, null, next);
+            expect(next).toBeCalledWith(error);
+        });
     });
 
-    it('should throw error (Conflict 409) if group exists #create', async () => {
-        const error = createError.Conflict();
-        req.body = { name: 'group123' };
-        groupService.createGroup.mockImplementation(() => Promise.reject(error));
-        await create(req, null, next);
-        expect(next).toBeCalledWith(error);
+    describe('#update function', () => {
+        it('should update group', async () => {
+            const updatedRows = [1];
+            req.body = { name: 'group123' };
+            req.params = { id: 'uuid' };
+            groupService.updateGroup.mockImplementation(() => Promise.resolve(updatedRows));
+            await update(req, res, null);
+            expect(groupService.updateGroup).toBeCalledWith(req.params.id, req.body);
+            expect(res.status).toBeCalledWith(200);
+        });
+
+        it('should return 204 status if group was not updated', async () => {
+            const updatedRows = [0];
+            req.body = { name: 'group123' };
+            req.params = { id: 'uuid' };
+            groupService.updateGroup.mockImplementation(() => Promise.resolve(updatedRows));
+            await update(req, res, null);
+            expect(groupService.updateGroup).toBeCalledWith(req.params.id, req.body);
+            expect(res.status).toBeCalledWith(204);
+        });
     });
 
-    it('should update group #update', async () => {
-        const updatedRows = [1];
-        req.body = { name: 'group123' };
-        req.params = { id: 'uuid' };
-        groupService.updateGroup.mockImplementation(() => Promise.resolve(updatedRows));
-        await update(req, res, null);
-        expect(groupService.updateGroup).toBeCalledWith(req.params.id, req.body);
-        expect(res.status).toBeCalledWith(200);
+    describe('#getAll function', () => {
+        it('should return groups', async () => {
+            const groups = ['group1', 'group2', 'group3'];
+            groupService.getAllGroups.mockImplementation(() => Promise.resolve(groups));
+            await getAll(null, res, null);
+            expect(res.json).toBeCalledWith(groups);
+            expect(res.status).toBeCalledWith(200);
+        });
     });
 
-    it('should return 204 status if group was not updated #update', async () => {
-        const updatedRows = [0];
-        req.body = { name: 'group123' };
-        req.params = { id: 'uuid' };
-        groupService.updateGroup.mockImplementation(() => Promise.resolve(updatedRows));
-        await update(req, res, null);
-        expect(groupService.updateGroup).toBeCalledWith(req.params.id, req.body);
-        expect(res.status).toBeCalledWith(204);
+    describe('#getById function', () => {
+        it('should return group by id', async () => {
+            const group = { name: 'group123' };
+            req.params = { id: 'uuid' };
+            groupService.getGroupById.mockImplementation(() => Promise.resolve(group));
+            await getById(req, res, null);
+            expect(groupService.getGroupById).toBeCalledWith(req.params.id);
+            expect(res.json).toBeCalledWith(group);
+            expect(res.status).toBeCalledWith(200);
+        });
+
+        it('should return 404 status if group does not exist', async () => {
+            const group = null;
+            req.params = { id: 'uuid' };
+            groupService.getGroupById.mockImplementation(() => Promise.resolve(group));
+            await getById(req, res, null);
+            expect(groupService.getGroupById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(404);
+        });
     });
 
-    it('should return groups #getAll', async () => {
-        const groups = ['group1', 'group2', 'group3'];
-        groupService.getAllGroups.mockImplementation(() => Promise.resolve(groups));
-        await getAll(null, res, null);
-        expect(res.json).toBeCalledWith(groups);
-        expect(res.status).toBeCalledWith(200);
+    describe('#remove function', () => {
+        it('should delete group by id', async () => {
+            const updatedRowsCount = 1;
+            req.params = { id: 'uuid' };
+            groupService.deleteGroupById.mockImplementation(() => Promise.resolve(updatedRowsCount));
+            await remove(req, res, null);
+            expect(groupService.deleteGroupById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(200);
+        });
+
+        it('should return 204 status if group was not deleted', async () => {
+            const updatedRowsCount = 0;
+            req.params = { id: 'uuid' };
+            groupService.deleteGroupById.mockImplementation(() => Promise.resolve(updatedRowsCount));
+            await remove(req, res, null);
+            expect(groupService.deleteGroupById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(204);
+        });
     });
 
-    it('should return group by id #getById', async () => {
-        const group = { name: 'group123' };
-        req.params = { id: 'uuid' };
-        groupService.getGroupById.mockImplementation(() => Promise.resolve(group));
-        await getById(req, res, null);
-        expect(groupService.getGroupById).toBeCalledWith(req.params.id);
-        expect(res.json).toBeCalledWith(group);
-        expect(res.status).toBeCalledWith(200);
-    });
+    describe('#addUsers function', () => {
+        it('should add users to group', async () => {
+            const group = { name: 'group123' };
+            req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
+            req.params = { id: 'uuid' };
+            groupService.addUsersToGroup.mockImplementation(() => Promise.resolve(group));
+            await addUsers(req, res, null);
+            expect(groupService.addUsersToGroup).toBeCalledWith(req.params.id, req.body.usersIds);
+            expect(res.status).toBeCalledWith(200);
+        });
 
-    it('should return 404 status if group does not exist #getById', async () => {
-        const group = null;
-        req.params = { id: 'uuid' };
-        groupService.getGroupById.mockImplementation(() => Promise.resolve(group));
-        await getById(req, res, null);
-        expect(groupService.getGroupById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(404);
-    });
+        it('should throw error (NotFound 404) if group does not exist', async () => {
+            const error = createError.NotFound();
+            req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
+            req.params = { id: 'uuid' };
+            groupService.addUsersToGroup.mockImplementation(() => Promise.reject(error));
+            await addUsers(req, null, next);
+            expect(next).toBeCalledWith(error);
+        });
 
-    it('should delete group by id #remove', async () => {
-        const updatedRowsCount = 1;
-        req.params = { id: 'uuid' };
-        groupService.deleteGroupById.mockImplementation(() => Promise.resolve(updatedRowsCount));
-        await remove(req, res, null);
-        expect(groupService.deleteGroupById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(200);
-    });
-
-    it('should return 204 status if group was not deleted #remove', async () => {
-        const updatedRowsCount = 0;
-        req.params = { id: 'uuid' };
-        groupService.deleteGroupById.mockImplementation(() => Promise.resolve(updatedRowsCount));
-        await remove(req, res, null);
-        expect(groupService.deleteGroupById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(204);
-    });
-
-    it('should add users to group #addUsers', async () => {
-        const group = { name: 'group123' };
-        req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
-        req.params = { id: 'uuid' };
-        groupService.addUsersToGroup.mockImplementation(() => Promise.resolve(group));
-        await addUsers(req, res, null);
-        expect(groupService.addUsersToGroup).toBeCalledWith(req.params.id, req.body.usersIds);
-        expect(res.status).toBeCalledWith(200);
-    });
-
-    it('should throw error (NotFound 404) if group does not exist #addUsers', async () => {
-        const error = createError.NotFound();
-        req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
-        req.params = { id: 'uuid' };
-        groupService.addUsersToGroup.mockImplementation(() => Promise.reject(error));
-        await addUsers(req, null, next);
-        expect(next).toBeCalledWith(error);
-    });
-
-    it('should throw error (BadRequest 400) if usersIds are not exist #addUsers', async () => {
-        const error = createError.BadRequest();
-        req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
-        req.params = { id: 'uuid' };
-        groupService.addUsersToGroup.mockImplementation(() => Promise.reject(error));
-        await addUsers(req, null, next);
-        expect(next).toBeCalledWith(error);
+        it('should throw error (BadRequest 400) if usersIds are not exist', async () => {
+            const error = createError.BadRequest();
+            req.body = { 'usersIds': ['user1_uuid', 'user2_uuid'] };
+            req.params = { id: 'uuid' };
+            groupService.addUsersToGroup.mockImplementation(() => Promise.reject(error));
+            await addUsers(req, null, next);
+            expect(next).toBeCalledWith(error);
+        });
     });
 });
--- api/controllers/__tests__/userController.test.js
+++ api/controllers/__tests__/userController.test.js
@@ -14,86 +14,97 @@ describe('User Controller Tests', () => {
         jest.clearAllMocks();
     });
 
-    it('should create a new user #create', async () => {
-        req.body = { login: 'user123', password: 'password123', age: 33 };
-        const user = { id: 'uuid', ...req.body };
-        userService.createUser.mockImplementation(() => Promise.resolve(user));
-        await create(req, res, null);
-        expect(res.json).toBeCalledWith(user.id);
-        expect(userService.createUser).toBeCalledWith(req.body);
-        expect(res.status).toBeCalledWith(201);
+    describe('#create function', () => {
+        it('should create a new user', async () => {
+            req.body = { login: 'user123', password: 'password123', age: 33 };
+            const user = { id: 'uuid', ...req.body };
+            userService.createUser.mockImplementation(() => Promise.resolve(user));
+            await create(req, res, null);
+            expect(res.json).toBeCalledWith(user.id);
+            expect(userService.createUser).toBeCalledWith(req.body);
+            expect(res.status).toBeCalledWith(201);
+        });
+
+        it('should throw error (Conflict 409) if user exists', async () => {
+            const error = createError.Conflict();
+            req.body = { login: 'user123', password: 'password123', age: 33 };
+            userService.createUser.mockImplementation(() => Promise.reject(error));
+            await create(req, null, next);
+            expect(next).toBeCalledWith(error);
+        });
     });
 
-    it('should throw error (Conflict 409) if user exists #create', async () => {
-        const error = createError.Conflict();
-        req.body = { login: 'user123', password: 'password123', age: 33 };
-        userService.createUser.mockImplementation(() => Promise.reject(error));
-        await create(req, null, next);
-        expect(next).toBeCalledWith(error);
+    describe('#update function', () => {
+        it('should update user', async () => {
+            const updatedRows = [1];
+            req.body = { login: 'user123', password: 'password123', age: 33 };
+            req.params = { id: 'uuid' };
+            userService.updateUser.mockImplementation(() => Promise.resolve(updatedRows));
+            await update(req, res, null);
+            expect(userService.updateUser).toBeCalledWith(req.params.id, req.body);
+            expect(res.status).toBeCalledWith(200);
+        });
+
+        it('should return 204 status if user was not updated', async () => {
+            const updatedRows = [0];
+            req.body = { login: 'user123', password: 'password123', age: 33 };
+            req.params = { id: 'uuid' };
+            userService.updateUser.mockImplementation(() => Promise.resolve(updatedRows));
+            await update(req, res, null);
+            expect(userService.updateUser).toBeCalledWith(req.params.id, req.body);
+            expect(res.status).toBeCalledWith(204);
+        });
     });
 
-    it('should update user #update', async () => {
-        const updatedRows = [1];
-        req.body = { login: 'user123', password: 'password123', age: 33 };
-        req.params = { id: 'uuid' };
-        userService.updateUser.mockImplementation(() => Promise.resolve(updatedRows));
-        await update(req, res, null);
-        expect(userService.updateUser).toBeCalledWith(req.params.id, req.body);
-        expect(res.status).toBeCalledWith(200);
+    describe('#getAll function', () => {
+        it('should return users', async () => {
+            const users = ['user1', 'user2', 'user3'];
+            userService.getAllUsers.mockImplementation(() => Promise.resolve(users));
+            await getAll(null, res, null);
+            expect(res.json).toBeCalledWith(users);
+            expect(res.status).toBeCalledWith(200);
+        });
     });
 
-    it('should return 204 status if user was not updated #update', async () => {
-        const updatedRows = [0];
-        req.body = { login: 'user123', password: 'password123', age: 33 };
-        req.params = { id: 'uuid' };
-        userService.updateUser.mockImplementation(() => Promise.resolve(updatedRows));
-        await update(req, res, null);
-        expect(userService.updateUser).toBeCalledWith(req.params.id, req.body);
-        expect(res.status).toBeCalledWith(204);
+
+    describe('#getById function', () => {
+        it('should return user by id', async () => {
+            const user = { login: 'user123', password: 'password123', age: 33 };
+            req.params = { id: 'uuid' };
+            userService.getUserById.mockImplementation(() => Promise.resolve(user));
+            await getById(req, res, null);
+            expect(userService.getUserById).toBeCalledWith(req.params.id);
+            expect(res.json).toBeCalledWith(user);
+            expect(res.status).toBeCalledWith(200);
+        });
+
+        it('should return 404 status if user does not exist', async () => {
+            const user = null;
+            req.params = { id: 'uuid' };
+            userService.getUserById.mockImplementation(() => Promise.resolve(user));
+            await getById(req, res, null);
+            expect(userService.getUserById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(404);
+        });
     });
 
-    it('should return users #getAll', async () => {
-        const users = ['user1', 'user2', 'user3'];
-        userService.getAllUsers.mockImplementation(() => Promise.resolve(users));
-        await getAll(null, res, null);
-        expect(res.json).toBeCalledWith(users);
-        expect(res.status).toBeCalledWith(200);
-    });
+    describe('#remove function', () => {
+        it('should delete user by id', async () => {
+            const updatedRowsCount = 1;
+            req.params = { id: 'uuid' };
+            userService.deleteUserById.mockImplementation(() => Promise.resolve(updatedRowsCount));
+            await remove(req, res, null);
+            expect(userService.deleteUserById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(200);
+        });
 
-    it('should return user by id #getById', async () => {
-        const user = { login: 'user123', password: 'password123', age: 33 };
-        req.params = { id: 'uuid' };
-        userService.getUserById.mockImplementation(() => Promise.resolve(user));
-        await getById(req, res, null);
-        expect(userService.getUserById).toBeCalledWith(req.params.id);
-        expect(res.json).toBeCalledWith(user);
-        expect(res.status).toBeCalledWith(200);
-    });
-
-    it('should return 404 status if user does not exist #getById', async () => {
-        const user = null;
-        req.params = { id: 'uuid' };
-        userService.getUserById.mockImplementation(() => Promise.resolve(user));
-        await getById(req, res, null);
-        expect(userService.getUserById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(404);
-    });
-
-    it('should delete user by id #remove', async () => {
-        const updatedRowsCount = 1;
-        req.params = { id: 'uuid' };
-        userService.deleteUserById.mockImplementation(() => Promise.resolve(updatedRowsCount));
-        await remove(req, res, null);
-        expect(userService.deleteUserById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(200);
-    });
-
-    it('should return 204 status if user was not deleted #remove', async () => {
-        const updatedRowsCount = 0;
-        req.params = { id: 'uuid' };
-        userService.deleteUserById.mockImplementation(() => Promise.resolve(updatedRowsCount));
-        await remove(req, res, null);
-        expect(userService.deleteUserById).toBeCalledWith(req.params.id);
-        expect(res.status).toBeCalledWith(204);
+        it('should return 204 status if user was not deleted', async () => {
+            const updatedRowsCount = 0;
+            req.params = { id: 'uuid' };
+            userService.deleteUserById.mockImplementation(() => Promise.resolve(updatedRowsCount));
+            await remove(req, res, null);
+            expect(userService.deleteUserById).toBeCalledWith(req.params.id);
+            expect(res.status).toBeCalledWith(204);
+        });
     });
 });
